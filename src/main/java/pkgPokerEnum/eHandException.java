ff77f4379a324b4b017564b3d74de3ec70f2a056
12965 <12965@DESKTOP-FLIBO64>
2017-04-05 02:28:28 -0400

    2

---
+++ src/main/java/pkgPokerEnum/eHandException.java
@@ -0,0 +1,20 @@
+package pkgPokerEnum;
+
+public class eHandException {
+
+	private int iHandException;
+	private eHandException(int iHandExceptionType){
+		
+		this.iHandException = iHandExceptionType;
+
+		
+	}
+	
+	public int getiHandException(){
+		
+		
+		return iHandException;
+		
+		
+	}
+}
